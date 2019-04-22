9c90a4c63679ae2e3582cebdc5735871807d87a9
Paula Alvaredo Gonzalez <paula.alvaredo@ALTIA.SANTIAGO>
2019-04-22 14:29:48 +0200

    prcatica 3

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,21 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
-@NgModule({
+
+@NgModule( {
   declarations: [HomeComponent, DashboardComponent],
-=======
-
-@NgModule({
-  declarations: [HomeComponent],
->>>>>>> 161a7b4616de2911258995cd1bf64d25d2a1914a
   imports: [
     CommonModule,
     HomeRoutingModule
   ]
-})
+} )
 export class HomeModule { }
