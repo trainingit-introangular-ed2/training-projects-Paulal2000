20c3b3a071a9550b51bc7159110f19403a79ffdf
Paula Alvaredo Gonzalez <paula.alvaredo@ALTIA.SANTIAGO>
2019-05-06 14:47:30 +0200

    Practica 5 -arreglos

--- src/app/projects/projects/projects.service.ts
+++ src/app/projects/projects/projects.service.ts
@@ -2,33 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class ProjectsService {
   private urlProjectsApi = 'https://api-base.herokuapp.com/api/pub/projects';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor( private httpClient: HttpClient ) { }
 
   public listarProyectos() {
-    return this.httpClient.get(this.urlProjectsApi);
+    return this.httpClient.get( this.urlProjectsApi );
   }
 
   public numProyectos() {
-    return this.httpClient.get(this.urlProjectsApi).pipe(map((proyectos: any) => proyectos.length));
+    return this.httpClient.get( this.urlProjectsApi ).pipe( map( ( proyectos: any ) => proyectos == null ? 0 : proyectos.length ) );
   }
 
-  public filtrarProyecto(filtro: any) {
+  public filtrarProyecto( filtro: any ) {
     return this.httpClient
-      .get(this.urlProjectsApi)
-      .pipe(map((projects: any) => projects.filter(project => project.name.toLowerCase().includes(filtro.name.toLowerCase()))));
+      .get( this.urlProjectsApi )
+      .pipe( map( ( projects: any ) => projects.filter( project => project.name.toLowerCase().includes( filtro.name.toLowerCase() ) ) ) );
   }
 
-  public creaProyecto(project: any) {
-    return this.httpClient.post(this.urlProjectsApi, project).pipe(map((projectCreado: any) => projectCreado._id));
+  public creaProyecto( project: any ) {
+    return this.httpClient.post( this.urlProjectsApi, project ).pipe( map( ( projectCreado: any ) => projectCreado._id ) );
   }
 
-  public obtieneProyecto(id: number) {
-    return this.httpClient.get(this.urlProjectsApi).pipe(map((projects: any) => projects.filter(project => project._id == id)));
+  public obtieneProyecto( id: number ) {
+    return this.httpClient.get( this.urlProjectsApi ).pipe( map( ( projects: any ) => projects.filter( project => project._id == id ) ) );
   }
 }
